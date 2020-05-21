db2e21c698272b606fbd55f67ca7edaca9ff4a51
Kalev Alpernas <kalev.alp@gmail.com>
2020-05-21 18:37:09 +0300

    updated benchmarks

--- notification-delay/sleep/watchtower/watchtower-monitor-trigger.js
+++ notification-delay/sleep/watchtower/watchtower-monitor-trigger.js
@@ -8,8 +8,13 @@ const uuidv4 = require('uuid/v4');
 async function triggerCheckerStateMachine(event) {
     const execid = uuidv4();
 
-    event.checkerFlow = "runOnce";
-    event.stabilityDelay = 7;
+    // Options: 'runTwice', 'runOnce'
+    event.checkerFlow = "runTwice";
+
+    event.stabilityDelay = 7; // In seconds
+
+    event.triggerStartTime = Date.now();
+
     const execParams = {
 	stateMachineArn,
 	input: JSON.stringify(event),
